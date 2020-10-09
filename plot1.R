4d6bcac12b843cbf1070380f7762e9973efe9e81
Amandeep Kaur <56528034+Amandeep1036@users.noreply.github.com>
2020-10-10 00:08:25 +0530

    Delete plo1.R

--- plot1.R
+++
@@ -1,8 +0,0 @@
-read_data<- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
-                      nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data <- subset(read_data, Date %in% c("1/2/2007","2/2/2007"))
-data$Date <- as.Date(data$Date, format="%d/%m/%Y")
-hist(data$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
-png("plot1.png", width=480, height=480)
-dev.off()
