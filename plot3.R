90f4317b0e3b27262a25552fa6fec64fe2a2b9d3
Amandeep Kaur <56528034+Amandeep1036@users.noreply.github.com>
2020-10-10 00:31:22 +0530

    Added plot3.R

---
+++ plot3.R
@@ -0,0 +1,14 @@
+read_data <- "household_power_consumption.txt"
+data <- read.table(read_data, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+sub_data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(sub_data$Date, sub_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+global_active_power <- as.numeric(subSetData$Global_active_power)
+subMetering_1 <- as.numeric(sub_data$Sub_metering_1)
+subMetering_2 <- as.numeric(sub_data$Sub_metering_2)
+subMetering_3 <- as.numeric(sub_data$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(datetime, subMetering_1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering_2, type="l", col="red")
+lines(datetime, subMetering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
